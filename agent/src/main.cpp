586abfe802070bc3922194007d9e22c3f1fca01a
codu07 <w.t.zhuque@hotmail.com>
2023-11-13 14:55:21 +0800

    first commit

---
+++ agent/src/main.cpp
@@ -0,0 +1,17 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2023 Wang Tao. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ /**
+ * @file main.cpp
+ * @author wangtao(wtzhuque@163.com)
+ * @date 2023/11/13 14:54:27
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
